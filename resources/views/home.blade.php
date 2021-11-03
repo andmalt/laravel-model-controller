96d491398acbaac0213524aaa120b774629c9a42
andmalt <andrea.maltempi@gmail.com>
2021-11-03 23:02:24 +0100

    inserted padding to cards

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,16 +3,16 @@
 @section('title','Home')
     
 @section('contain')
-    <div class="container">
+    <div class="container p-4">
         
             <div class="row">
                 @foreach ($movies as $movie)
                 <div class="col-12 col-md-6 col-lg-3 card">
-                    <h1>{{ $movie['title'] }}</h1>
-                    <h3>{{ $movie['original_title'] }}</h2>
-                    <p>{{ $movie['nationality'] }}</p>
-                    <p>{{ $movie['date'] }}</p>
-                    <p>{{ $movie['vote'] }}</p>
+                    <h3>Titolo: {{ $movie['title'] }}</h3>
+                    <h5>Titolo Originale: {{ $movie['original_title'] }}</h2>
+                    <p>Nazionalità; {{ $movie['nationality'] }}</p>
+                    <h6>Data di uscita: {{ $movie['date'] }}</h6>
+                    <p>Voto: {{ $movie['vote'] }}</p>
                 </div> 
                 @endforeach
             </div>
