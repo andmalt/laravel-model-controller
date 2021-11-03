8d1377345c8545994b146a93c3e96d73a6d9ba07
andmalt <andrea.maltempi@gmail.com>
2021-11-03 18:14:17 +0100

    inserted Model

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,11 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Movie;
 
 class HomeController extends Controller
 {
     public function index(){
 
+        $movie = Movie::all();
+
+        var_dump($movie);
         return view('home');
     }
 }
